3fc713f8f7a16583c9efef2ebd8bb872fa78d078
TAKTAK Hajer <taktakhajer@gmail.com>
2021-10-03 19:30:40 +0100

    ExtensionClass

---
+++ Atelier2Et3CSharp-DMWM/ExtensionClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atelier2Et3CSharp_DMWM
+{
+    public static class ExtensionClass
+    {
+        public static double TotalPrix(this CartProducts cart)
+        {
+            double total = 0;
+            foreach(Produit prod in cart.produits)
+            {
+                total += prod.Prix;
+            }
+            return total;
+        }
+    }
+}
